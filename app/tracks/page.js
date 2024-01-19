457da4b1a4bcd477836a46915731c9f159fc704d
Jeevant Verma <jverma_be23@thapar.edu>
2024-01-19 20:48:05 +0530

    Made a new data.json file and imported contetnt from cards from it instead of implementing loop

--- app/tracks/page.js
+++ app/tracks/page.js
@@ -1,16 +1,20 @@
 "use client"
 import Link from 'next/link';
 import styles from './tracks.module.css';
+import Content from './data.json'
 
 export default function Page() {
-  const generateEventDiv = (eventId, title, description) => (
-    <Link key={eventId} href={`/tracks/problem_statements/problem${eventId}`} passHref>
-      <div className={`${styles.Ev} ${styles.Event}`}>
-        <h1 className={styles.heading}>{title}</h1>
-        <p className={styles.para}>{description}</p>
-      </div>
-    </Link>
-  );
+  const generateEventDiv = (event) => {
+    const { id, title, description } = event;
+    return (
+      <Link key={id} href={`/tracks/problem_statements/problem${id}`} passHref>
+        <div className={`${styles.Ev} ${styles.Event}`}>
+          <h1 className={styles.heading}>{title}</h1>
+          <p className={styles.para}>{description}</p>
+        </div>
+      </Link>
+    );
+  };
 
   return (
     <>
@@ -19,15 +23,7 @@ export default function Page() {
         <p className={styles.para}>--------------------------------------------------------</p>
         <div>
           <div className={styles.container}>
-            {(() => {
-              const result = [];
-              for (let i = 1; i <= 4; i++) {
-                result.push(
-                  generateEventDiv(`${i}`, `Event ${i}`, `Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero deserunt, doloremque tenetur cupiditate exercitationem temporibus tempore eligendi maiores, provident ut quos, facere velit ratione necessitatibus voluptas ullam inventore dolores sit.`)
-                );
-              }
-              return result;
-            })()}
+            {Content.map((event) => generateEventDiv(event))}
           </div>
         </div>
       </div>
